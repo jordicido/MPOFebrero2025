ffafc60508b04fb85614db5df9b47304db7c05c7
jordicido <jordi.cidoncha4@gmail.com>
2025-11-20 17:15:43 +0100

    Modelos

--- proyecto_final/main.py
+++ proyecto_final/main.py
@@ -14,12 +14,6 @@ def main():
                     city = input_city()
                     lat, long, country = get_geolocation(city)
                     fore_data = get_forecast(lat, long)
-                    print(f"Tiempo de la ciudad: {city} País: {country}")
-                    print(f"Temperatura máxima de hoy: {fore_data.daily.temperature_2m_max}")
-                    print(f"Temperatura mínima de hoy: {fore_data.daily.temperature_2m_min}")
-                    print(f"Hora del amanecer: {fore_data.daily.sunrise}")
-                    print(f"Hora del anochecer: {fore_data.daily.sunset}")
-                    print(f"Probabilidad de lluvia: {fore_data.daily.precipitation_probability_max}%")
 
 
                 case 0:
